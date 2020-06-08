70c2da1ebdc8445363b9c8188cbc9ca884f41d0a
Muntakim <muntakim.rahman@gmail.com>
2020-06-08 03:01:12 -0700

    Working TitleScreen

---
+++ Assets/MainMenu.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+// Required for Changing Scenes
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    // PlayButton Functionality
+    // Must be Public so can be called from pressing PlayButton
+    public void PlayGame ()
+    {
+        // Load Next Scene in Queue.
+        // In this case, this is the actual Game.
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    // QuitButton Functionality
+    // Must be Public so can be called from pressing QuitButton
+    public void QuitGame(){
+
+        // For Functionality in Unity Editor
+        Debug.Log("Quit Game.");
+        // End Gameplay
+        Application.Quit();
+    }
+}
